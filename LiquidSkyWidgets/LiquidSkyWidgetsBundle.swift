ff5f31fd8b7b06a95be981a2733cbad894be267d
Acxtrilla <cazz9584@gmail.com>
2025-08-19 01:16:26 +0100

    Add initial widget support with LiquidSkyWidgetsBundle and associated assets
    This commit introduces the LiquidSkyWidgetsBundle, which serves as the main entry point for the widget extension. It includes the Info.plist configuration for the widget and sets up the necessary asset files, including AppIcon and color sets for accent and background. These additions lay the groundwork for future widget development and customization.

---
+++ LiquidSkyWidgets/LiquidSkyWidgetsBundle.swift
@@ -0,0 +1,18 @@
+//
+//  LiquidSkyWidgetsBundle.swift
+//  LiquidSkyWidgets
+//
+//  Created by Callum Matthews on 19/08/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiquidSkyWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        LiquidSkyWidgets()
+        LiquidSkyWidgetsControl()
+        LiquidSkyWidgetsLiveActivity()
+    }
+}
